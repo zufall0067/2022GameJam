51366ddb62a1d21952d3b95f0d45b8c305890759
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-07-11 17:34:11 +0900

    no message

--- Assets/01. Scripts/Background/BackgroundManager.cs
+++ Assets/01. Scripts/Background/BackgroundManager.cs
@@ -7,34 +7,43 @@ public class BackgroundManager : MonoBehaviour
 {
     [SerializeField] // 배경을 변경하는데 걸리는 시간
     private float[] floorChangeDelay;
+    //위 배열 크기 받는 애
+    private int floorCount;
+
+    [SerializeField] //배경 게임 오브젝트 
+    private GameObject[] backgroundGameObject;
+    //위 배열 크기 받는 애
+    private int backgroundGameObjectCount;
 
     //지나간 시간
     private float nowTime;
     //지금 켜져있는 배경 오브젝트
     private int nowActiveBackgroundObject;
-    //배경 게임 오브젝트 
-    public GameObject[] backgroundGameObject;
+    
 
 
 
     private void Start()
     {
-        Instantiate(backgroundGameObject[0], this.gameObject.transform);
         nowTime = 0f;
         nowActiveBackgroundObject = 0;
+        floorCount = floorChangeDelay.Length;
+        backgroundGameObjectCount = backgroundGameObject.Length;
+        Instantiate(backgroundGameObject[0], this.gameObject.transform);
+        
     }
 
     private void Update()
     {
         nowTime += Time.deltaTime;
 
-        if(nowTime > floorChangeDelay[nowActiveBackgroundObject])
+        if(nowTime > floorChangeDelay[nowActiveBackgroundObject] && backgroundGameObjectCount > nowActiveBackgroundObject)
         {
             nowActiveBackgroundObject++;
             nowTime = 0f;
 
             Instantiate(backgroundGameObject[nowActiveBackgroundObject], this.gameObject.transform);
-            this.gameObject.transform.GetChild(0).GetComponent<Material>().DOFade(0,1);
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
