23898a3bb6bbd992acb337746497216693cc6e99
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-07-11 19:58:08 +0900

    07 11 좆됫다 ㅎㅎ

--- Assets/01. Scripts/Enemy/Enemy.cs
+++ Assets/01. Scripts/Enemy/Enemy.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Enemy : PoolableMono
 {
-    public int hp;
+    public int hp; //프로퍼티로 빼야될듯..41번줄
     public int atk;
+
+    public float movingX = 0;
+    public float movingY = 0;
 
     public Transform targetTrm;
 
@@ -13,11 +18,27 @@ public class Enemy : PoolableMono
     {
         targetTrm = GameObject.Find("Tower").transform;
         Shooting();
+        Moving();
+    }
+
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            PoolManager.Instance.Push(this);
+        }
+    }
+
+    protected void Moving()
+    {
+        //movingY += 0.1f;
+        //transform.position = new Vector2(movingX, movingY);
     }
 
     public override void Reset()
     {
-        throw new System.NotImplementedException();
+        transform.DOComplete();
+        hp = 10; //존나귀찮네
     }
 
     public virtual void Shooting()
