0a6a5fa9f44ea1d7ad6962882b5a76f806d445d2
StraightBet <slupgames1025@gmail.com>
2022-07-12 10:58:20 +0900

    UI 베이스 작업중

---
+++ Assets/01. Scripts/UI/UI_SkillSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_SkillSelect : MonoBehaviour
+{
+    public struct Struct
+    {
+        public Image image;
+        public Struct(Image _image)
+        {
+            image = _image;
+        }
+
+        public void DebugLog()
+        {
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
