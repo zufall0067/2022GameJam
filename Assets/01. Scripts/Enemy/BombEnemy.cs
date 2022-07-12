3f0ef58da49194dd27da7835d20d6561967160c8
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-07-12 10:58:18 +0900

    07 12 권준서뭐함

--- Assets/01. Scripts/Enemy/BombEnemy.cs
+++ Assets/01. Scripts/Enemy/BombEnemy.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalEnemy : Enemy
+public class BombEnemy : Enemy
 {
-    //필요한것  :  공격력, 체력 바, 히트확인, 플레이어위치, 죽었을때 연료
     public override void Shooting()
     {
         //InvokeRepeating("Fire", 1f, 2.5f);
 
-        //Fire();
+        Fire();
     }
 
     public override void Fire()
@@ -20,11 +19,21 @@ public class NormalEnemy : Enemy
         Bullet bullet = PoolManager.Instance.Pop("EnemyBullet") as Bullet;
         bullet.transform.position = transform.position;
         bullet.dir = dir;
-        
+
         bullet.Shoot();
     }
     public override void HPbar()
     {
-        base.HPbar();
+        //base.HPbar();
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("BULLET"))
+        {
+            Fire();
+        }
+    }
+
+
 }
