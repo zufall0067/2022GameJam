94d8f8f18ac4319f8aae9d6aecde4b79fa16721f
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-07-12 18:54:12 +0900

    정배꺼 지워!

--- Assets/01. Scripts/Enemy/Enemy.cs
+++ Assets/01. Scripts/Enemy/Enemy.cs
@@ -19,7 +19,7 @@ public class Enemy : PoolableMono
     public Transform targetTrm;
 
 
-    void Start()
+    protected void Awake()
     {
         hpBar = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
         targetTrm = GameObject.Find("Tower").transform;
@@ -50,13 +50,13 @@ public class Enemy : PoolableMono
     {
         transform.DOComplete();
         CancelInvoke();
-        hp = 100; //����������
+        
 
     }
 
     public virtual void Shooting()
     {
-
+        
     }
 
     public virtual void Fire()
