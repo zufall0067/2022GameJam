cf10969a824a8c6bc994693db2327b585f587e1b
StraightBet <slupgames1025@gmail.com>
2022-07-12 13:59:30 +0900

    재장ㅇㄹㄴㄹㅇㅇㄴㄹ

--- Assets/01. Scripts/Tower.cs
+++ Assets/01. Scripts/Tower.cs
@@ -12,30 +12,54 @@ public class Tower : MonoBehaviour
 
     Vector2 mousePos;
     public Camera Camera;
-    
 
-
+    public int bulletCount = 0;
+    public bool isReloading = false;
+    public float reloadCount = 0;
+    public float overReloadCount = 1f;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         FuelDecrease();
 
-        if (Input.GetMouseButtonDown(0) && !isSkilling)
+        if (Input.GetMouseButtonDown(0) && !isSkilling && !isReloading)
         {
             if (SkillManager.Instance.isSkill == true) return;
             mousePos = Input.mousePosition;
             mousePos = Camera.ScreenToWorldPoint(mousePos);
 
-            Vector2 dir = mousePos - new Vector2(0, -1); //ÃÑ¾Ë ¹ß»ç À§Ä¡ /*(Vector2)transform.position*/;
+            Vector2 dir = mousePos - new Vector2(0, -1); //ï¿½Ñ¾ï¿½ ï¿½ß»ï¿½ ï¿½ï¿½Ä¡ /*(Vector2)transform.position*/;
             dir.Normalize();
 
             Bullet bullet = PoolManager.Instance.Pop("Bullet") as Bullet;
             bullet.dir = dir;
             bullet.Shoot();
+            bulletCount++;
+        }
+
+        if (bulletCount >= 10 || Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isReloading)
+            {
+                Debug.Log("Reload Start");
+                isReloading = true;
+            }
+        }
+
+        if (isReloading)
+        {
+            reloadCount += Time.deltaTime;
+            if (reloadCount >= overReloadCount)
+            {
+                Debug.Log("Reload end");
+                bulletCount = 0;
+                reloadCount = 0;
+                isReloading = false;
+            }
         }
 
         UIManager.Instance.fuelText.text = fuel.ToString();
@@ -46,7 +70,7 @@ public class Tower : MonoBehaviour
         if (fuel < 0) return;
         fuel -= Time.deltaTime * 30f;
 
-        if (fuel < 0) Debug.Log("µÚÁü");
+        if (fuel < 0) Debug.Log("ï¿½ï¿½ï¿½ï¿½");
     }
 
     private void Fire()
