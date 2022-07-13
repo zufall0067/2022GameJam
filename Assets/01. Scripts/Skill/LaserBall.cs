3a4489e3a8a0a34c1f65e7aaa50c270f8fdae8d3
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-07-13 11:58:26 +0900

    레이저 완성? 아마도 ㅋㅋ

--- Assets/01. Scripts/Skill/LaserBall.cs
+++ Assets/01. Scripts/Skill/LaserBall.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+Ôªøusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,9 +11,10 @@ public class LaserBall : Skill
 
     public override void Select()
     {
+        CancelInvoke("IsReadyfalse");
         isReady = true;
         Invoke("IsReadyfalse", 4f);
-        Debug.Log("∑π¿Ã¿˙ ¿Â¬¯");
+        Debug.Log("Î†àÏù¥Ï†Ä Ïû•Ï∞©");
     }
 
 
@@ -23,26 +24,36 @@ public class LaserBall : Skill
         {
             SkillManager.Instance.SkillPanelQuit();
             lineRenderer.SetActive(true);
-            MeshCollider meshCollider = lineRenderer.AddComponent<MeshCollider>();
-            Mesh mesh = new Mesh();
-            lineRenderer.GetComponent<LineRenderer>().BakeMesh(mesh, true);
-            meshCollider.sharedMesh = mesh;
         }
 
         if(isReady && Input.GetMouseButton(0))
         {
             mousePos = Input.mousePosition;
             mousePos = Camera.ScreenToWorldPoint(mousePos);
+            Vector2 dir = mousePos - new Vector2(0, -1); //*(Vector2)transform.position*/;
+            dir.Normalize();
+
             //Vector2 dir = mousePos - (Vector2)transform.position; // /*(Vector2)transform.position*/;
             //dir.Normalize();
 
-            Draw2DRay(lineRenderer.transform.position, mousePos * 80);
+            Draw2DRay(lineRenderer.transform.position, dir * 80);
 
-
+            RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(0, -1), dir, 30);
+            Debug.DrawRay(new Vector2(0, -1), dir * 30, Color.red);
+            //Debug.Log(hit.collider);
+            for(int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+                if (hit.collider.transform.CompareTag("ENEMY"))
+                {
+                    Debug.Log("ÏóêÎÑàÎØ∏ÏûáÏñ¥Ïöî!");
+                    hit.collider.GetComponent<Enemy>().hp -= 1f;
+                }
+            }
             //tower.isSkilling = false;
         }
-        
-        if(!isReady && Input.GetMouseButton(0) || Input.GetMouseButtonUp(0))
+
+        if (!isReady && Input.GetMouseButton(0) || Input.GetMouseButtonUp(0))
         {
             lineRenderer.SetActive(false);
         }
@@ -51,7 +62,7 @@ public class LaserBall : Skill
 
     private void Draw2DRay(Vector2 position, Vector2 point)
     {
-        lineRenderer.GetComponent<LineRenderer>().SetPosition(0,new Vector3(0,-1,0));
+        lineRenderer.GetComponent<LineRenderer>().SetPosition(0,new Vector2(0,-1));
         lineRenderer.GetComponent<LineRenderer>().SetPosition(1, point);
     }
 
