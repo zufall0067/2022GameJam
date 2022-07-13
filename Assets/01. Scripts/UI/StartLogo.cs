6bc22e009b4ac9e7acc796e6db09ce8cf06d7130
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-07-13 20:31:42 +0900

    no message

---
+++ Assets/01. Scripts/UI/StartLogo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StartLogo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject TMPro;
+
+
+    void Start()
+    {
+        TMPro.transform.DOScale(new Vector3(1, 1, 1), 0.5f).SetDelay(0.5f);
+        StartCoroutine(DG());
+    }
+
+    private IEnumerator DG()
+    {
+        while (true)
+        {
+
+            TMPro.transform.DOLocalJump(new Vector3(0, 1.5f), 1, 1, 1f);
+
+            TMPro.transform.DOPunchRotation(new Vector3(Random.Range(-80f, 80), 0), 1f);
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    public void DestroyText()
+    {
+        Destroy(TMPro);
+    }
+}
