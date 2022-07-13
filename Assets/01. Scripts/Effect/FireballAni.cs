e797ac6ab8bbb3a0622f05d9b290fb9930bd98f7
StraightBet <slupgames1025@gmail.com>
2022-07-13 10:34:23 +0900

    파이어볼 폭파 복구

---
+++ Assets/01. Scripts/Effect/FireballAni.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAni : MonoBehaviour
+{
+    public void DestroyThis()
+    {
+        Destroy(this.gameObject);
+    }
+}
